e8413cab50ad175b5b71461d38f377f14e8a807e
Alberto Camarena <albertocamarena.77t@gmail.com>
2019-10-01 23:04:21 -0500

    Solucion a los 4 retos

--- src/index.js
+++ src/index.js
@@ -1,38 +1,61 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://rickandmortyapi.com/api/character/";
+// const API = "https://us-central1-escuelajs-api.cloudfunctions.net/characters";
+var counter = 0;
+const MaxPage = 2;
+localStorage.clear();
 
 const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
+  return fetch(api).then(response => response.json());
+};
+
+const renderData = function(data) {
+  const next = data.info.next;
+  localStorage.setItem("next_fetch", next);
+  console.log(localStorage.getItem("next_fetch"));
+
+  const characters = data.results;
+  let output = characters
+    .map(character => {
+      return `
       <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+    <img src="${character.image}" />
+    <h2>${character.name}<span>${character.species}</span></h2>
+  </article>
+  `;
     })
-    .catch(error => console.log(error));
-}
+    .join("");
+  let newItem = document.createElement("section");
+  newItem.classList.add("Items");
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
+};
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = async function(url) {
+  const data = await getData(url);
+  renderData(data);
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      if (localStorage.getItem("next_fetch") && counter < MaxPage) {
+        counter++;
+        let next_fetch = localStorage.getItem("next_fetch");
+        loadData(next_fetch);
+      } else if (counter === MaxPage) {
+        alert("Se acabaron los personajes");
+        intersectionObserver.unobserve($observe);
+      } else {
+        loadData(API);
+      }
+      console.log(counter);
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px"
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+);
 
 intersectionObserver.observe($observe);
